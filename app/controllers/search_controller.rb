442e551f288f7d454f2ec8c90e2e781c4695a8fa
Max Glassie <msglassie@gmail.com>
2017-04-20 12:04:51 -0600

    complete'

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
 
   def index
+    @event_count = EventService.new.events(params["q"])["total_items"]
     @events = Event.find_by_zip(params["q"])
   end
 
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,6 @@ class Event
   def self.find_by_zip(zip)
     service = EventService.new
     data = service.events(zip)
-    binding.pry
     events = data["events"]["event"].map do |event|
       Event.new(event)
     end
@@ -12,7 +11,6 @@ class Event
   attr_reader :url, :city_name, :start_time, :description, :title, :venue_address, :venue_name
 
   def initialize(data)
-    binding.pry
     @url = data["url"]
     @city_name = data["city_name"]
     @start_time = data["start_time"]
@@ -20,13 +18,5 @@ class Event
     @title = data["title"]
     @venue_address = data["venue_address"]
     @venue_name = data["venue_name"]
-  end
-
-  def nil_guard(data)
-    if data.nil?
-      "Not provided"
-    else
-      data
-    end
   end
 end
