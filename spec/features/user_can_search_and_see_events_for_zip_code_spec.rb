2b09cb0120666f1c2f493aa149a0b6df6155fb5c
Max Glassie <msglassie@gmail.com>
2017-04-20 12:15:16 -0600

    searches for 'q' that
    sneeeeeaky little letter.

--- spec/features/user_can_search_and_see_events_for_zip_code_spec.rb
+++ spec/features/user_can_search_and_see_events_for_zip_code_spec.rb
@@ -6,13 +6,15 @@ RSpec.feature "user can search and see events for the zip code" do
     visit search_path
 
     save_and_open_page
-    fill_in("Search by zip...", with: "80202")
+    fill_in("q", with: "80202")
     click_on("Find Events")
 
     expect(current_path).to eq(search_path)
 
-    expect(page).to have_content("135 events for March")
-    expect(page).to have_content("start_time")
-    expect(page).to have_content("description")
+    expect(page).to have_content("288 events for March")
+    expect(page).to have_content("SF Comedy Showcase")
+    expect(page).to have_content("2017-03-05 20:00:00")
+    expect(page).to have_content("Punch Line - San Francisco")
+    expect(page).to have_content("444 Battery Street")
   end
 end
