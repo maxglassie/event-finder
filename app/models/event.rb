f9a398bd4b6c189dd67241837e358f38f9a13345
Max Glassie <msglassie@gmail.com>
2017-04-20 12:00:43 -0600

    everything is in the view except event count

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,23 +3,23 @@ class Event
   def self.find_by_zip(zip)
     service = EventService.new
     data = service.events(zip)
-    events = data["events"].map do |event|
+    binding.pry
+    events = data["events"]["event"].map do |event|
       Event.new(event)
     end
-    binding.pry
   end
 
   attr_reader :url, :city_name, :start_time, :description, :title, :venue_address, :venue_name
 
   def initialize(data)
     binding.pry
-    @url = data[1][0]["url"]
-    @city_name = data[1][0]["city_name"]
-    @start_time = data[1][0]["start_time"]
-    @description = data[1][0]["description"]
-    @title = data[1][0]["title"]
-    @venue_address = data[1][0]["venue_address"]
-    @venue_name = data[1][0]["venue_name"]
+    @url = data["url"]
+    @city_name = data["city_name"]
+    @start_time = data["start_time"]
+    @description = data["description"]
+    @title = data["title"]
+    @venue_address = data["venue_address"]
+    @venue_name = data["venue_name"]
   end
 
   def nil_guard(data)
